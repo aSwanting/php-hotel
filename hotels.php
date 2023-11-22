f6271249d5e5e4ec2a934ac9bcf59148268c8e84
aSwanting <gab.damico@gmail.com>
2023-11-22 15:58:42 +0100

    add: basic bootstrap styling

--- hotels.php
+++ hotels.php
@@ -58,45 +58,55 @@ $hotels = [
         <title>PHP Hotel Table</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     </head>
-    <body>
-        
-<table class="table">
-    <thead>
-        <tr>
-            <th scope="col">Name</th>
-            <th scope="col">Description</th>
-            <th scope="col">Parking</th>
-            <th scope="col">Rating</th>
-            <th scope="col">Distance to center</th>
-        </tr>
-    </thead>
-    <tbody>                
-        <?php foreach ($hotels as $hotel) { ?>
-            <tr>
+    <body>     
 
-                <?php foreach ($hotel as $key => $hotelInfo) { ?>
-                    <td>
+    <div class="container p-5">
+    <h3>Hotel Listing</h3>
 
-                        <?php 
+    <div class="table-wrapper p-3 border border-1 rounded-4 overflow-hidden">
 
-                            if ($key == "parking") {
-                                echo $hotelInfo ?  "Available" :  "Not available";
+        <!-- Table Start -->
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">Name</th>
+                        <th scope="col">Description</th>
+                        <th scope="col">Parking</th>
+                        <th scope="col">Rating</th>
+                        <th scope="col">Distance to center</th>
+                    </tr>
+                </thead>
+                <tbody>                
+                    <?php foreach ($hotels as $hotel) { ?>
+                        <tr>
 
-                            } else if ($key == "distance_to_center") {
-                                echo $hotelInfo . " km" ;   
+                            <?php foreach ($hotel as $key => $hotelInfo) { ?>
+                                <td>
 
-                            } else {
-                                echo $hotelInfo ;
-                            }
-                            
-                        ?> 
+                                    <?php 
 
-                    </td>
-                <?php } ?>
-            </tr>
-        <?php } ?>
-    </tbody>
-</table>
+                                        if ($key == "parking") {
+                                            echo $hotelInfo ?  "Available" :  "Not available";
+
+                                        } else if ($key == "distance_to_center") {
+                                            echo $hotelInfo . " km";   
+
+                                        } else {
+                                            echo $hotelInfo;
+                                        }
+
+                                    ?> 
+
+                                </td>
+                            <?php } ?>
+                        </tr>
+                    <?php } ?>
+                </tbody>
+            </table>
+            <!-- Table End -->
+
+        </div>
+    </div>
 
 
 </body>
